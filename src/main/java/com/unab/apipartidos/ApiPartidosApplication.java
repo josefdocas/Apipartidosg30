1008c039899517fd3f5b575cf9411b7e86b117b9
Jose Fdo <josefdocas@gmail.com>
2022-11-09 21:07:31 -0500

    Primer Commit

---
+++ src/main/java/com/unab/apipartidos/ApiPartidosApplication.java
@@ -0,0 +1,14 @@
+package com.unab.apipartidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPartidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPartidosApplication.class, args);
+		System.out.println("Api Corriendo...");
+	}
+
+}
---
+++ src/test/java/com/unab/apipartidos/ApiPartidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unab.apipartidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPartidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
